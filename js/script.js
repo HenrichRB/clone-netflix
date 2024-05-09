ac1bd5c3c108cb97cffd5d913b5a5f1beeff540d
Henrich Ribeiro <henrichrb@gmail.com>
2024-05-09 13:12:50 -0300

    criando o fetch e fazendo chamadas
    arquitetando o esqueleto das chamadas a api e a criação de elementos para exibição

--- js/script.js
+++ js/script.js
@@ -4,7 +4,23 @@ let movies = [];
 
 fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-br`)
     .then((response) => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const movies = data.results;
+        const $movieContainer = document.getElementById('movie-container');
+
+        movies.forEach((movie) => {
+            const {title, poster_path, overview} = movie;
+            const movieElement = document.createElement('div');
+            movieElement.classList.add('movie');
+            movieElement.innerHTML = `
+            <h2>${title}</h2>
+            <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title} Poster">
+            <p>${overview}</p>
+            `;
+
+            $movieContainer.appendChild(movieElement);
+        });
+    })
     .catch(error => {
         console.error('Ocorreu um erro ao obter os filmes populares', error);
     })
