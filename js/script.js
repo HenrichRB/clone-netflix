018c111a4f00a4b5e1200fdff785964eea101fbe
Henrich Ribeiro <henrichrb@gmail.com>
2024-05-13 15:56:38 -0300

    feat: create a modal to show sinopse

--- js/script.js
+++ js/script.js
@@ -9,13 +9,33 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-
         const $movieContainer = document.getElementById('movie-container');
 
         movies.forEach((movie) => {
-            const {title, poster_path, overview} = movie;
+            const {id, title, poster_path, overview} = movie;
             const movieElement = document.createElement('div');
             movieElement.classList.add('movie');
             movieElement.innerHTML = `
-            <h2>${title}</h2>
-            <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title} Poster">
-            <p>${overview}</p>
+            
+            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal-${id}">
+                    <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title} Poster">
+                </button>
+
+                <div class="modal fade modal-lg my-modal" id="exampleModal-${id}" tabindex="-1" aria-labelledby="exampleModalLabel-${id}" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content bg-meu">
+                            <div class="modal-header d-flex align-items-center justify-content-center">
+                                <h1 class="modal-title fs-5 w-100" id="exampleModalLabel-${id}">${title}</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title} Poster">
+                                <p>${overview}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             `;
 
             $movieContainer.appendChild(movieElement);
